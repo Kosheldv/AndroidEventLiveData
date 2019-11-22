fee8232c005aff8c1a50e7f94c34ad470b2df92f
DKoshel <dkoshel@diasoft.ru>
2019-11-22 19:17:07 +0300

    PostValue in MainThread

--- EventLiveData/src/main/java/com/kosheldv/eventlivedata/EventLiveData.kt
+++ EventLiveData/src/main/java/com/kosheldv/eventlivedata/EventLiveData.kt
@@ -3,6 +3,7 @@ package com.kosheldv.eventlivedata
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.support.annotation.MainThread
 
 open class EventLiveData<T> : ViewModel() {
 
@@ -11,6 +12,7 @@ open class EventLiveData<T> : ViewModel() {
     val eventLiveData : LiveData<Event<T>>
         get() = _eventLiveData
 
+    @MainThread
     fun postValue(content: T) {
         _eventLiveData.value = Event(content)  // Trigger the event by setting a new Event as a new value
     }
